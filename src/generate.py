c0d1236e55fbb9e2f14f082d6b4aa08650d97302
KoreanWienerSchnizel <kwschnizel@gmail.com>
2025-04-01 20:21:40 -0700

    adding a docs directory for github pages to display the static site

--- src/generate.py
+++ src/generate.py
@@ -3,7 +3,7 @@ from os.path import isdir, isfile
 from block_utils import extract_title, markdown_to_html_node
 
 
-def generate_page(source_path, template_path, dest_path):
+def generate_page(source_path, template_path, dest_path, base_path):
     print(f"Generating page from {source_path} to {dest_path} using {template_path}")
 
     with open(source_path) as f:
@@ -17,6 +17,8 @@ def generate_page(source_path, template_path, dest_path):
     source_html = markdown_to_html_node(source).to_html()
     html = template.replace("{{ Title }}", title)
     html = html.replace("{{ Content }}", source_html)
+    html = html.replace('href="/', f'href="{base_path}')
+    html = html.replace('src="/', f'src="{base_path}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as f:
@@ -24,12 +26,12 @@ def generate_page(source_path, template_path, dest_path):
         f.close()
 
 
-def generate_pages_recursive(content_dir_path, template_path, dest_dir_path):
+def generate_pages_recursive(content_dir_path, template_path, dest_dir_path, base_path):
     for file in os.listdir(content_dir_path):
         file_path = os.path.join(content_dir_path, file)
         dest_path = os.path.join(dest_dir_path, file)
         if os.path.isdir(file_path):
-            generate_pages_recursive(file_path, template_path, dest_path)
+            generate_pages_recursive(file_path, template_path, dest_path, base_path)
         elif os.path.isfile(file_path) and file.endswith(".md"):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(file_path, template_path, dest_path)
+            generate_page(file_path, template_path, dest_path, base_path)
--- src/main.py
+++ src/main.py
@@ -1,18 +1,32 @@
 import os
+import sys
 import shutil
 from file_utils import copy_directory
-from generate import generate_page, generate_pages_recursive
+from generate import generate_pages_recursive
 
 
 def main():
     static_dir = "./static"
     public_dir = "./public"
-    content_dir = "./content/"
+    docs_dir = "./docs"
+    content_dir = "./content"
     template_path = "template.html"
+    if len(sys.argv) < 2:
+        base_path = "/"
+    else:
+        base_path = sys.argv[1]
 
-    shutil.rmtree(public_dir)
+    # Generate pages for local testing
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
     copy_directory(static_dir, public_dir)
-    generate_pages_recursive(content_dir, template_path, public_dir)
+    generate_pages_recursive(content_dir, template_path, public_dir, "/")
+
+    # Generate pages for github pages
+    if os.path.exists(docs_dir):
+        shutil.rmtree(docs_dir)
+    copy_directory(static_dir, docs_dir)
+    generate_pages_recursive(content_dir, template_path, docs_dir, base_path)
 
 
 if __name__ == "__main__":
